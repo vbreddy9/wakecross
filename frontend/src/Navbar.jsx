8b76aa0b21bb327cdfe62c2b3ecaaba7cc62fb98
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-04-05 17:41:07 +0530

    Update Navbar.jsx

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -4,14 +4,14 @@ import mlogo from "./assets/logo-w.png";
 
 const Navbar = () => {
   return (
-    <header className="absolute top-0 left-0 w-full z-50 bg-white">
-     <div className="container mx-auto flex flex-row items-center justify-between gap-2 px-4 sm:px-6 py-2 sm:py-4">
+    <header className="sticky top-0 left-0 w-full z-50 bg-white shadow-md">
+      <div className="container mx-auto flex items-center justify-between px-4 py-4">
         {/* Logo */}
         <a href="/">
           <img
             src={mlogo}
-            alt="wake cross familty"
-            className="w-30 h-16 sm:w-44 sm:h-20"
+            alt="wake cross family"
+            className="w-28 h-14 sm:w-44 sm:h-20"
           />
         </a>
 
