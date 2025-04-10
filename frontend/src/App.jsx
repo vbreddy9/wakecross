55c2cee7be9554e53c936329cec419d283e25ec8
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-04-10 17:01:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import Navbar from './Navbar';
 import HeroSection from './HeroSection';
 import Footer from './Footer';
@@ -8,22 +10,28 @@ import ReviewCard from './ReviewCard.jsx';
 import WorryFreeSection from './WorryFreeSection.jsx';
 import InsuranceInfo from './InsuranceInfo.jsx';
 import DentalFAQ from './DentalFAQ.jsx';
+import ThankYouPage from './Thankyou.jsx'; // <-- Import it
 
-
+const HomePage = () => (
+  <>
+    <Navbar />
+    <HeroSection />
+    <RegistrationForm />
+    <ReviewCard />
+    <WorryFreeSection />
+    <InsuranceInfo />
+    <DentalFAQ />
+    <ContactDetails />
+    <Footer />
+  </>
+);
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <RegistrationForm/>
-      <ReviewCard/>
-      <WorryFreeSection/>
-      <InsuranceInfo/>
-      <DentalFAQ/>
-      <ContactDetails/>
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/thankyou" element={<ThankYouPage />} />
+    </Routes>
   );
 };
 
