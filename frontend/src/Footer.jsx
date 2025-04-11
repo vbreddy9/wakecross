3d02cdbb7f82c41355d5a4479f9824021a972394
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-04-11 15:03:21 +0530

    Update Footer.jsx

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
+
 const Footer = () => {
   return (
-    <footer className="py-4 text-black text-center">
+    <footer className="footer">
       <p>&copy; 2025 Wakecross Family Dentistry. All Rights Reserved.</p>
     </footer>
   );
