7214ad0051db6f79fa874433e88ef087e3e070b1
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-04-18 23:50:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -112,7 +112,7 @@ app.post("/send-email", (req, res) => {
                     <p><strong>Full Name:</strong></strong> ${firstName} ${lastName}</p>
                     <p><strong>Phone:</strong> ${formattedPhone}</p>
                     <p>If your request is urgent, please call us at 
-                      <a href="tel:+101902921475" style="color:#684749; text-decoration:none;"><strong><u>+1 0190 292 1475</u></strong></a>.
+                      <a href="tel:+19194530777" style="color:#684749; text-decoration:none;"><strong><u>(919)-453-0777</u></strong></a>.
                     </p>
                     <br/>
                     <p>Warm regards,<br/><strong>Wakecross Family Team</strong></p>
