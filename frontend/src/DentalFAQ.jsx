276d956bbe6eb1bc265f82dfe8da303bd7480578
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-04-20 13:07:29 +0530

    Update DentalFAQ.jsx

--- frontend/src/DentalFAQ.jsx
+++ frontend/src/DentalFAQ.jsx
@@ -36,7 +36,7 @@ const DentalFAQ = () => {
     <div className="faq-section">
       <div className="faq-container">
         <h2 className="faq-heading">
-          Frequently Asked <span className="highlight">Questions</span>
+          Frequently Asked <span className="regular">Quetions</span>
         </h2>
 
         <div className="underline"></div>
